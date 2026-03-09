0a172d33f762d606fb6a65f455d919bc2d9fabda
khakhathumamoalosi-ui <khakhathumamoalosi@gmail.com>
2026-03-09 18:09:11 +0200

    Add files via upload

---
+++ python.py
@@ -0,0 +1,17 @@
+school= dict(Program="BSCITY1S2",Module="Python",Age=28)
+
+for dict_key, dict_value in school.items():
+    print(dict_key, dict_value)
+    
+cars={"Model":"2026","brand":"toyota"}
+print(cars['Model'])
+cars['milige']=4000
+
+colors={"blue","orange"}
+print("orange" in 'colors')
+colors.add('apple')
+
+school= int ("input(enter the number of key_value pairs you want to add: "                                                                                                                                                                                                                                                                                                                                        
+
+
+
